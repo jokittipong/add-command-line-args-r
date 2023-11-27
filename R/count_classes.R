dd0f8bbfe2c42d337b3d8e255e48da6414cb9d56
Kittipong Wongwipasamitkun <tojokittipong@gmail.com>
2023-11-27 12:25:00 -0800

    DSCI-522 Milestone3 InClass Exercise R

--- R/count_classes.R
+++ R/count_classes.R
@@ -1,15 +1,27 @@
-library(tidyverse)
+# author: Kittipong Wongwipasamitkun
+# date: 2023-11-27
 
-main <- function() {
+"This script counts the classes for a specified column
+from specified file_path
+
+Usage: count_classes.R <file_path> <class_col> <output_file>
+" -> doc
+
+library(tidyverse)
+library(docopt)
+
+opt <- docopt(doc)
+
+main <- function(file_path, class_col, output_file) {
   # read in wisconsin breast cancer data
-  data <- read_csv('data/cancer_train.csv')
+  data <- read_csv(opt$file_path)
 
   result <- data |> 
-    group_by(diagnosis) |> 
+    group_by(opt$class_col) |> 
     summarize(Count = n()) |> 
     rename_at(1, ~ "class")
 
-  write.csv(result, 'results/cancer/class_count.csv')
+  write.csv(result, opt$output_file)
 }
 
 main()
